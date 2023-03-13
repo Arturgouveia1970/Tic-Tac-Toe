90787ee6ccd79881840f3294265d0b90cb9b9ad0
arturgouveia1970 <artgouveiag@gmail.com>
2023-03-13 22:11:01 +0000

    changed line breaks

---
+++ .history/src/components/App_20230313220232.js
@@ -0,0 +1,47 @@
+/* eslint-disable react/jsx-no-bind */
+import React, { useState } from 'react';
+import Board from './Board';
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to move #${move}`;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      // eslint-disable-next-line
+      <li key={move}>
+        <button type="button" onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    // eslint-disable-next-line
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
